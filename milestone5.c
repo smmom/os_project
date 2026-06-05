6e50a087e2b1704a0af078b28e0b9f098fb7db43
jinanrasas <jinanra@post.jce.ac.il>
2026-06-05 20:36:57 +0300

    the animation is smoother

--- milestone5.c
+++ milestone5.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <signal.h>
 #include "raylib.h"
 #include "dijkstra.h"
 
@@ -16,8 +17,6 @@
 #define ENTITY_RADIUS 10
 #define MAX_TRAVELERS 10
 
-bool playing = true;
-
 typedef struct {
     pid_t pid;
     int arrivedAt;
@@ -26,23 +25,17 @@ typedef struct {
 } IPCMessage;
 
 // Manual Vector2 Helper Functions
-Vector2 MyVector2Sub(Vector2 v1, Vector2 v2)
-{ return (Vector2){ v1.x - v2.x, v1.y - v2.y }; }
-
-Vector2 MyVector2Add(Vector2 v1, Vector2 v2)
-{ return (Vector2){ v1.x + v2.x, v1.y + v2.y }; }
-
-Vector2 MyVector2Scale(Vector2 v, float scale)
-{ return (Vector2){ v.x * scale, v.y * scale }; }
-
-float MyVector2Len(Vector2 v)
-{ return sqrtf(v.x * v.x + v.y * v.y); }
-
+Vector2 MyVector2Sub(Vector2 v1, Vector2 v2) { return (Vector2){ v1.x - v2.x, v1.y - v2.y }; }
+Vector2 MyVector2Add(Vector2 v1, Vector2 v2) { return (Vector2){ v1.x + v2.x, v1.y + v2.y }; }
+Vector2 MyVector2Scale(Vector2 v, float scale) { return (Vector2){ v.x * scale, v.y * scale }; }
+float MyVector2Len(Vector2 v) { return sqrtf(v.x * v.x + v.y * v.y); }
 Vector2 MyVector2Norm(Vector2 v) {
     float length = MyVector2Len(v);
-    if (length > 0) 
-        return (Vector2) { v.x / length, v.y / length };
+    if (length > 0) return (Vector2){ v.x / length, v.y / length };
     return (Vector2){ 0, 0 };
+}
+Vector2 MyVector2Lerp(Vector2 v1, Vector2 v2, float amount) {
+    return (Vector2){ v1.x + amount * (v2.x - v1.x), v1.y + amount * (v2.y - v1.y) };
 }
 
 typedef struct {
@@ -58,11 +51,19 @@ typedef struct Traveler {
     Color color;
     bool active;
     Vector2 entityPosition;
+
+    // Animation state
+    int currentFromNode;
+    int currentToNode;
+    float moveProgress; // 0.0 to 1.0
+    float moveDuration; // Seconds
 } Traveler;
 
 Traveler travelers[MAX_TRAVELERS];
 int numTravelers = 0;
 NodeGUI nodePositions[MAX_NODES];
+enum { STATE_IDLE, STATE_RUNNING, STATE_PAUSED } animationState = STATE_IDLE;
+int pipefd[2];
 
 void calculateNodePositions(int numNodes) {
     float angle_step = 2 * PI / numNodes;
@@ -94,8 +95,7 @@ void DrawGraph(Graph* graph) {
     for (int i = 0; i < graph->numNodes; i++) {
         DrawCircleV(nodePositions[i].position, NODE_RADIUS, LIGHTGRAY);
         DrawCircleLines(nodePositions[i].position.x, nodePositions[i].position.y, NODE_RADIUS, DARKGRAY);
-        char nodeText[5]; 
-        sprintf(nodeText, "%d", i);
+        char nodeText[5]; sprintf(nodeText, "%d", i);
         DrawText(nodeText, nodePositions[i].position.x - MeasureText(nodeText, FONT_SIZE) / 2, nodePositions[i].position.y - FONT_SIZE / 2, FONT_SIZE, BLACK);
     }
     for (int i = 0; i < numTravelers; i++) {
@@ -107,112 +107,131 @@ void DrawGraph(Graph* graph) {
             DrawCircleV(travelers[i].entityPosition, ENTITY_RADIUS, travelers[i].color);
         }
     }
+
+    Color btnColor = GREEN;
+    char* btnText = "PLAY";
+    if (animationState == STATE_RUNNING) { btnColor = RED; btnText = "PAUSE"; }
+    else if (animationState == STATE_PAUSED) { btnColor = GREEN; btnText = "PLAY"; }
+
+    DrawRectangle(10, 10, 100, 30, btnColor);
+    DrawText(btnText, 10 + (100 - MeasureText(btnText, 20)) / 2, 15, 20, WHITE);
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
-        return 1; }
-    FILE* file = fopen(argv[1], "r");
-    int numNodes, numEdges;
-    fscanf(file, "%d %d", &numNodes, &numEdges);
-    Graph* graph = createGraph(numNodes);
-
-    for (int i = 0; i < numEdges; i++) {
-        int u, v, w; fscanf(file, "%d %d %d", &u, &v, &w);
-        addEdge(graph, u, v, w);
-    }
-    calculateNodePositions(numNodes);
-    fscanf(file, "%d", &numTravelers);
-    Color colors[] = {BLUE, GREEN, YELLOW, MAGENTA, ORANGE, LIME, SKYBLUE, VIOLET, BEIGE, BROWN};
-
-    int pipefd[2]; pipe(pipefd);
-
-    fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
-
+void StartTravelers(Graph* graph) {
     for (int i = 0; i < numTravelers; i++) {
-        fscanf(file, "%d %d", &travelers[i].startNode, &travelers[i].endNode);
-        travelers[i].color = colors[i % 10]; 
-        travelers[i].active = true;
-        travelers[i].entityPosition = nodePositions[travelers[i].startNode].position;
         pid_t pid = fork();
-
-        if (pid == 0) {
+        if (pid == 0) { // Child
             close(pipefd[0]);
-
             int p[MAX_NODES], len, w;
-
             if (dijkstra(graph, travelers[i].startNode, travelers[i].endNode, p, &len, &w)) {
                 for (int j = 0; j < len; j++) {
                     IPCMessage m = { getpid(), p[j], (j < len - 1) ? p[j+1] : -1, (j == len - 1) };
                     write(pipefd[1], &m, sizeof(IPCMessage));
                     if (j < len - 1) {
                         int weight = 0; Edge* e = graph->adjList[p[j]].head;
-                        while (e) {
-                            if (e->destination == p[j+1]) {
-                            weight = e->weight; break;
-                        }
-                            e = e->next; }
+                        while (e) { if (e->destination == p[j+1]) { weight = e->weight; break; } e = e->next; }
                         usleep(weight * 300000);
                     }
                 }
             }
-            IPCMessage f = { getpid(), -1, -1, true };
-            write(pipefd[1], &f, sizeof(IPCMessage));
-            close(pipefd[1]);
-            exit(0);
-        }
-        else { travelers[i].pid = pid; }
+            IPCMessage f = { getpid(), -1, -1, true }; write(pipefd[1], &f, sizeof(IPCMessage));
+            close(pipefd[1]); exit(0);
+        } else { travelers[i].pid = pid; }
     }
-    fclose(file); close(pipefd[1]);
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Graph Visualization - Milestone 5");
+}
+
+void PauseTravelers() {
+    for (int i = 0; i < numTravelers; i++) if (travelers[i].pid > 0) kill(travelers[i].pid, SIGSTOP);
+}
+
+void ResumeTravelers() {
+    for (int i = 0; i < numTravelers; i++) if (travelers[i].pid > 0) kill(travelers[i].pid, SIGCONT);
+}
+
+void KillAllTravelers() {
+    for (int i = 0; i < numTravelers; i++) {
+        if (travelers[i].pid > 0) {
+            kill(travelers[i].pid, SIGKILL);
+            waitpid(travelers[i].pid, NULL, 0);
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) { fprintf(stderr, "Usage: %s <input_file>\n", argv[0]); return 1; }
+    FILE* file = fopen(argv[1], "r");
+    int numNodes, numEdges; fscanf(file, "%d %d", &numNodes, &numEdges);
+    Graph* graph = createGraph(numNodes);
+    for (int i = 0; i < numEdges; i++) { int u, v, w; fscanf(file, "%d %d %d", &u, &v, &w); addEdge(graph, u, v, w); }
+    calculateNodePositions(numNodes);
+    fscanf(file, "%d", &numTravelers);
+    Color colors[] = {BLUE, GREEN, YELLOW, MAGENTA, ORANGE, LIME, SKYBLUE, VIOLET, BEIGE, BROWN};
+    for (int i = 0; i < numTravelers; i++) {
+        fscanf(file, "%d %d", &travelers[i].startNode, &travelers[i].endNode);
+        travelers[i].color = colors[i % 10]; travelers[i].active = true;
+        travelers[i].entityPosition = nodePositions[travelers[i].startNode].position;
+        travelers[i].pid = 0; travelers[i].pathLength = 0;
+        travelers[i].currentFromNode = travelers[i].startNode;
+        travelers[i].currentToNode = travelers[i].startNode;
+        travelers[i].moveProgress = 1.0f;
+    }
+    fclose(file);
+    pipe(pipefd); fcntl(pipefd[0], F_SETFL, O_NONBLOCK);
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Graph Visualization - Milestone 5 ");
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
-        IPCMessage m;
-
-        /* ===== BUTTON ===== */
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-            Vector2 m = GetMousePosition();
-            if (CheckCollisionPointRec(m,(Rectangle){10,10,80,30}))
-                playing = !playing;
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 10, 100, 30})) {
+            if (animationState == STATE_IDLE) { animationState = STATE_RUNNING; StartTravelers(graph); }
+            else if (animationState == STATE_RUNNING) { animationState = STATE_PAUSED; PauseTravelers(); }
+            else if (animationState == STATE_PAUSED) { animationState = STATE_RUNNING; ResumeTravelers(); }
         }
 
-        while (read(pipefd[0], &m, sizeof(IPCMessage)) > 0) {
-            for (int i = 0; i < numTravelers; i++) {
-                if (travelers[i].pid == m.pid) {
-                    if (m.finished && m.arrivedAt == -1) {
-                        travelers[i].active = false;
-                        printf("[PID=%d] finished\n", m.pid); 
-                    }
-                    else {
-                        if (m.nextNode == -1)
-                            printf("[PID=%d] arrived at node %d | DESTINATION\n", m.pid, m.arrivedAt);
+        if (animationState == STATE_RUNNING) {
+            IPCMessage m;
+            while (read(pipefd[0], &m, sizeof(IPCMessage)) > 0) {
+                for (int i = 0; i < numTravelers; i++) {
+                    if (travelers[i].pid == m.pid) {
+                        if (m.finished && m.arrivedAt == -1) { travelers[i].active = false; printf("[PID=%d] finished\n", m.pid); }
+                        else {
+                            if (m.nextNode == -1) printf("[PID=%d] arrived at node %d | DESTINATION\n", m.pid, m.arrivedAt);
+                            else printf("[PID=%d] arrived at node %d | next node: %d\n", m.pid, m.arrivedAt, m.nextNode);
 
-                        else
-                            printf("[PID=%d] arrived at node %d | next node: %d\n", m.pid, m.arrivedAt, m.nextNode);
+                            if (travelers[i].pathLength == 0) { int dw; dijkstra(graph, travelers[i].startNode, travelers[i].endNode, travelers[i].path, &travelers[i].pathLength, &dw); }
 
-                        if (travelers[i].pathLength == 0) {
-                            int dw; 
-                            dijkstra(graph, travelers[i].startNode, travelers[i].endNode, travelers[i].path, &travelers[i].pathLength, &dw);
+                            // Start smooth move to next node
+                            if (m.nextNode != -1) {
+                                travelers[i].currentFromNode = m.arrivedAt;
+                                travelers[i].currentToNode = m.nextNode;
+                                travelers[i].moveProgress = 0.0f;
+                                // Calculate duration based on edge weight
+                                int weight = 0; Edge* e = graph->adjList[m.arrivedAt].head;
+                                while (e) { if (e->destination == m.nextNode) { weight = e->weight; break; } e = e->next; }
+                                travelers[i].moveDuration = weight * 0.3f; // Same as usleep factor
+                            } else {
+                                travelers[i].entityPosition = nodePositions[m.arrivedAt].position;
+                                travelers[i].moveProgress = 1.0f;
+                            }
                         }
-                        travelers[i].entityPosition = nodePositions[m.arrivedAt].position;
                     }
                 }
             }
+
+            // Update smooth positions
+            float dt = GetFrameTime();
+            for (int i = 0; i < numTravelers; i++) {
+                if (travelers[i].active && travelers[i].moveProgress < 1.0f) {
+                    travelers[i].moveProgress += dt / travelers[i].moveDuration;
+                    if (travelers[i].moveProgress > 1.0f) travelers[i].moveProgress = 1.0f;
+                    travelers[i].entityPosition = MyVector2Lerp(
+                        nodePositions[travelers[i].currentFromNode].position,
+                        nodePositions[travelers[i].currentToNode].position,
+                        travelers[i].moveProgress
+                    );
+                }
+            }
         }
-        // button
-        DrawRectangle(10,10,80,30, playing?RED:GREEN);
-        DrawText(playing?"STOP":"PLAY",20,15,20,WHITE);
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawGraph(graph);
-        EndDrawing();
+        BeginDrawing(); ClearBackground(RAYWHITE); DrawGraph(graph); EndDrawing();
     }
-    for (int i = 0; i < numTravelers; i++)
-        waitpid(travelers[i].pid, NULL, 0);
-
-    freeGraph(graph);
-    CloseWindow();
-    return 0;
+    KillAllTravelers(); freeGraph(graph); CloseWindow(); return 0;
 }
